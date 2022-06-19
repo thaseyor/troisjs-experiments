2a6271d016383e3fec08142b09fe39762cebd29b
thaseyor <mikeston@mail.ru>
2022-06-19 23:04:10 +0600

    feat: home link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
+  <home-button />
   <router-view></router-view>
 </template>
 
 <script setup>
 import { RouterView } from "vue-router";
+import HomeButton from "@/components/HomeButton.vue";
 </script>
---
+++ src/components/HomeButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <template v-if="show">
+    <router-link class="home-button" :to="{ name: 'home' }">
+      <div style="color: white">{{ "<" }}</div>
+    </router-link>
+  </template>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import { RouterLink, useRoute } from "vue-router";
+const route = useRoute();
+
+const show = computed(() => route.name !== "home");
+</script>
+
+<style>
+.home-button {
+  position: fixed;
+  top: 8px;
+  left: 8px;
+  color: white;
+  font-size: x-large;
+}
+</style>
